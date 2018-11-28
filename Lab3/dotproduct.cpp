7b25d7365f1b879f259502d9f25d215b37c20231
Malcolm Wigren <trekommafem2@gmail.com>
2018-11-28 09:59:05 +0100

    Start lab3

--- Lab3/dotproduct.cpp
+++ Lab3/dotproduct.cpp
@@ -11,15 +11,15 @@
 
 /* SkePU user functions */
 
-/*
-float userfunction(...)
+float mult(float x1, float x2)
 {
-	// your code here
+    return x1*x2;
 }
 
-// more user functions...
-
-*/
+float plus(float x1, float x2)
+{
+    return x1 + x2;
+}
 
 
 int main(int argc, const char* argv[])
@@ -29,43 +29,48 @@ int main(int argc, const char* argv[])
 		std::cout << "Usage: " << argv[0] << " <input size> <backend>\n";
 		exit(1);
 	}
-	
+
 	const size_t size = std::stoul(argv[1]);
 	auto spec = skepu2::BackendSpec{skepu2::Backend::typeFromString(argv[2])};
 //	spec.setCPUThreads(<integer value>);
-	
-	
+
+
 	/* Skeleton instances */
-//	auto instance = skepu2::Map<???>(userfunction);
+	auto instance = skepu2::MapReduce<2>(mult, plus);
+	auto mapInstance = skepu2::Map<2>(mult);
+	auto reduceInstance = skepu2::Reduce(plus);
 // ...
-	
+
 	/* Set backend (important, do for all instances!) */
 //	instance.setBackend(spec);
-	
+
 	/* SkePU containers */
 	skepu2::Vector<float> v1(size, 1.0f), v2(size, 2.0f);
-	
-	
+
+
 	/* Compute and measure time */
 	float resComb, resSep;
-	
+
 	auto timeComb = skepu2::benchmark::measureExecTime([&]
 	{
-		// your code here
+        resComb = instance(v1, v2);
 	});
 	
 	auto timeSep = skepu2::benchmark::measureExecTime([&]
 	{
 		// your code here
+        skepu2::Vector<float> mapResult(size);
+        mapInstance(mapResult, v1, v2);
+        resSep = reduceInstance(mapResult);
 	});
-	
+
 	std::cout << "Time Combined: " << (timeComb.count() / 10E6) << " seconds.\n";
 	std::cout << "Time Separate: " << ( timeSep.count() / 10E6) << " seconds.\n";
-	
-	
+
+
 	std::cout << "Result Combined: " << resComb << "\n";
 	std::cout << "Result Separate: " << resSep  << "\n";
-	
+
 	return 0;
 }
 
